247198917fccd169d9d8b22de0bb91e8970db7f7
%Valfredoqs% <valfredosilva@gmail.com>
2019-05-18 14:04:37 -0300

    Correção segundo exercicio

--- app/src/main/java/mobile/senaigo/com/meuretrofit2/resource/UserResource.java
+++ app/src/main/java/mobile/senaigo/com/meuretrofit2/resource/UserResource.java
@@ -1,15 +1,15 @@
 package mobile.senaigo.com.meuretrofit2.resource;
 
-        import java.util.List;
+import java.util.List;
 
-        import mobile.senaigo.com.meuretrofit2.model.User;
-        import retrofit2.Call;
-        import retrofit2.http.Body;
-        import retrofit2.http.DELETE;
-        import retrofit2.http.GET;
-        import retrofit2.http.POST;
-        import retrofit2.http.PUT;
-        import retrofit2.http.Path;
+import mobile.senaigo.com.meuretrofit2.model.User;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface UserResource {
 
